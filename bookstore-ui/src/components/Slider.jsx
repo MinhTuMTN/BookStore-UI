81cca8b4807fa03820ff12c45f8539555d00a8d1
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2023-04-23 23:17:17 +0700

    Hoàn thành Slider

--- bookstore-ui/src/components/Slider.jsx
+++ bookstore-ui/src/components/Slider.jsx
@@ -1,66 +1,86 @@
-import React from "react";
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@mui/icons-material";
+import { useState } from "react";
 import styled from "styled-components";
-import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
-import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import { sliderItems } from "../data";
+import { mobile } from "../responsive";
 
 const Container = styled.div`
-  margin: 20px 0px;
   width: 100%;
+  height: 75vh;
+  margin: 10px 0px;
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
   position: relative;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  ${mobile({ display: "none" })}
 `;
 
-const LeftArrow = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 9%;
-  width: 30px;
-  height: 30px;
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: #fff7f7;
   border-radius: 50%;
   display: flex;
-  justify-content: center;
   align-items: center;
-  cursor: pointer;
-  background-color: #fff5ee;
-`;
-
-const RightArrow = styled.div`
+  justify-content: center;
   position: absolute;
-  top: 50%;
-  right: 9%;
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
   cursor: pointer;
-  background-color: #fff5ee;
+  opacity: 0.5;
+  z-index: 2;
 `;
 
-const SliderContent = styled.div`
-  width: 80%;
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
 `;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : sliderItems.length - 1);
+    } else {
+      setSlideIndex(slideIndex < sliderItems.length - 1 ? slideIndex + 1 : 0);
+    }
+  };
+
   return (
     <Container>
-      <LeftArrow>
-        <ArrowBackIosNewOutlinedIcon />
-      </LeftArrow>
-      <SliderContent>
-        <img
-          width="100%"
-          src="https://cdn0.fahasa.com/media/magentothem/banner7/MSAT_mainbanner-T4-840x320.jpg"
-          alt=""
-        />
-      </SliderContent>
-      <RightArrow>
-        <ArrowForwardIosOutlinedIcon />
-      </RightArrow>
+      <Arrow direction="left" onClick={() => handleClick("left")}>
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item, index) => (
+          <Slide key={`slider-${index}`}>
+            <Image src={item} />
+          </Slide>
+        ))}
+      </Wrapper>
+      <Arrow direction="right" onClick={() => handleClick("right")}>
+        <ArrowRightOutlined />
+      </Arrow>
     </Container>
   );
 };
