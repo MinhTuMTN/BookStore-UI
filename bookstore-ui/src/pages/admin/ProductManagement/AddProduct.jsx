bc723dd1cf7f08da0fcf891b962dee62e9ce477f
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2023-04-28 16:22:36 +0700

    Chức năng thêm sách

--- bookstore-ui/src/pages/admin/ProductManagement/AddProduct.jsx
+++ bookstore-ui/src/pages/admin/ProductManagement/AddProduct.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { InfoItem, InfoItemLabel, Right } from "../../Profile";
 import Sidebar from "../../../components/sidebar/Sidebar";
 import styled from "styled-components";
-import { colors } from "../../../data";
+import { colors, endpoint } from "../../../data";
+import { useNavigate } from "react-router";
+import Cookies from "js-cookie";
+import ErrorMessage from "../../../components/ErrorMessage";
 
 export const Title = styled.span`
   font-weight: bold;
@@ -37,129 +40,130 @@ export const Button = styled.div`
   cursor: pointer;
 `;
 const AddProduct = () => {
-//   const [data, setData] = useState({});
-//   const navigate = useNavigate();
+  const [data, setData] = useState({});
+  const navigate = useNavigate();
 
-//   const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-//   useEffect(() => {}, [errorMessage]);
+  useEffect(() => {}, [errorMessage]);
 
-//   useEffect(() => {
-//     fetch(`${endpoint}/user/profile`, {
-//       headers: {
-//         authorization: Cookies.get("authToken"),
-//       },
-//     })
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setData(data);
-//       })
-//       .catch((error) => console.error(error));
-//   }, []);
-
-//   const handleUpdate = () => {
-//     fetch(`${endpoint}/user/profile`, {
-//       method: "PUT",
-//       headers: {
-//         "Content-Type": "application/json",
-//         authorization: Cookies.get("authToken"),
-//       },
-//       body: JSON.stringify(data),
-//     })
-//       .then((response) => {
-//         if (response.status == 200) {
-//           navigate("/profile");
-//           return;
-//         }
-//       })
-//       .catch((error) => {
-//         setErrorMessage("Đã có lỗi xảy ra. Vui lòng thử lại");
-//       });
-//   };
+  const handleCreateBook = () => {
+    fetch(`${endpoint}/admin/books/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: Cookies.get("authToken"),
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.status == 200) {
+          navigate("/admin/books");
+          return;
+        } else {
+          setErrorMessage("Đã có lỗi xảy ra. Vui lòng thử lại");
+        }
+      })
+      .catch((error) => {
+        setErrorMessage("Đã có lỗi xảy ra. Vui lòng thử lại");
+      });
+  };
 
   return (
     <div className="list">
       <Sidebar />
 
-          <Right
-            style={{ alignItems: "flex-start", justifyContent: "flex-start" }}
-          >
-            <Title>Chỉnh Sửa Thông Tin Sản Phẩm</Title>
-            <Form>
-            <InfoItem>
-                <InfoItemLabel>Đường dẫn hình ảnh</InfoItemLabel>
-                <FormInput
-                  placeholder="http://"
-                //   value={data.address}
-                //   onChange={(e) =>
-                //     setData((prevData) => ({
-                //       ...prevData,
-                //       address: e.target.value,
-                //     }))
-                //   }
-                />
-              </InfoItem>
-              <InfoItem>
-                <InfoItemLabel>Tiêu đề</InfoItemLabel>
-                <FormInput
-                  placeholder={"Cuốn sách"}
-                //   value={data.username}
-                //   onChange={(e) =>
-                //     setData((prevData) => ({
-                //       ...prevData,
-                //       username: e.target.value,
-                //     }))
-                //   }
-                />
-              </InfoItem>
-              <InfoItem>
-                <InfoItemLabel>Tác giả</InfoItemLabel>
-                <FormInput
-                  placeholder={"Nguyễn Văn A"}
-                //   value={data.full_name}
-                //   onChange={(e) =>
-                //     setData((prevData) => ({
-                //       ...prevData,
-                //       full_name: e.target.value,
-                //     }))
-                //   }
-                />
-              </InfoItem>
-              <InfoItem>
-                <InfoItemLabel>Giá tiền</InfoItemLabel>
-                <FormInput
-                  placeholder="VNĐ"
-                //   value={data.address}
-                //   onChange={(e) =>
-                //     setData((prevData) => ({
-                //       ...prevData,
-                //       address: e.target.value,
-                //     }))
-                //   }
-                />
-              </InfoItem>
-              <InfoItem>
-                <InfoItemLabel>Mô tả</InfoItemLabel>
-                <FormInput
-                  placeholder="Cuốn sách hay"
-                //   value={data.address}
-                //   onChange={(e) =>
-                //     setData((prevData) => ({
-                //       ...prevData,
-                //       address: e.target.value,
-                //     }))
-                //   }
-                />
-              </InfoItem>
-              <ButtonWrapper>
-                <Button
-                //  onClick={handleUpdate}
-                 >
-                    Cập nhật thông tin
-                </Button>
-              </ButtonWrapper>
-            </Form>
-          </Right>
+      <Right style={{ alignItems: "flex-start", justifyContent: "flex-start" }}>
+        <Title>Chỉnh Sửa Thông Tin Sản Phẩm</Title>
+        <ErrorMessage
+          errorMessage={errorMessage}
+          display={errorMessage == "" ? "none" : "flex"}
+        />
+
+        <Form>
+          <InfoItem>
+            <InfoItemLabel>Đường dẫn hình ảnh</InfoItemLabel>
+            <FormInput
+              placeholder="http://"
+              value={data.image}
+              onChange={(e) =>
+                setData((prevData) => ({
+                  ...prevData,
+                  image: e.target.value,
+                }))
+              }
+            />
+          </InfoItem>
+          <InfoItem>
+            <InfoItemLabel>Tiêu đề</InfoItemLabel>
+            <FormInput
+              placeholder={"Cuốn sách"}
+              value={data.title}
+              onChange={(e) =>
+                setData((prevData) => ({
+                  ...prevData,
+                  title: e.target.value,
+                }))
+              }
+            />
+          </InfoItem>
+          <InfoItem>
+            <InfoItemLabel>Tác giả</InfoItemLabel>
+            <FormInput
+              placeholder={"Nguyễn Văn A"}
+              value={data.author}
+              onChange={(e) =>
+                setData((prevData) => ({
+                  ...prevData,
+                  author: e.target.value,
+                }))
+              }
+            />
+          </InfoItem>
+          <InfoItem>
+            <InfoItemLabel>Giá tiền</InfoItemLabel>
+            <FormInput
+              placeholder="VNĐ"
+              value={data.price}
+              onChange={(e) =>
+                setData((prevData) => ({
+                  ...prevData,
+                  price: e.target.value,
+                }))
+              }
+            />
+          </InfoItem>
+          <InfoItem>
+            <InfoItemLabel>Mô tả</InfoItemLabel>
+            <FormInput
+              placeholder="Cuốn sách hay"
+              value={data.description}
+              onChange={(e) =>
+                setData((prevData) => ({
+                  ...prevData,
+                  description: e.target.value,
+                }))
+              }
+            />
+          </InfoItem>
+          <InfoItem>
+            <InfoItemLabel>Ngày xuất bản</InfoItemLabel>
+            <FormInput
+              type="date"
+              value={data.publication_date}
+              onChange={(e) =>
+                setData((prevData) => ({
+                  ...prevData,
+                  publication_date: e.target.value,
+                }))
+              }
+            />
+          </InfoItem>
+          <ButtonWrapper>
+            <Button onClick={handleCreateBook}>Cập nhật thông tin</Button>
+          </ButtonWrapper>
+        </Form>
+      </Right>
     </div>
   );
 };
